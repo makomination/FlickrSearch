7c185ab9ca61119916e9ac2f85323821a3ab3a63
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-07-16 11:41:02 -0700

    Finished 1st tutorial

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Makoto Ishihara on 2017-07-16.
+//  Copyright © 2017 Makomination. All rights reserved.
+//
+
+import UIKit
+class FlickrPhotoCell: UICollectionViewCell{
+    @IBOutlet var imageView: UIImageView!
+}
